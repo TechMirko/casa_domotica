0ebc69d0314ddc32440f05bf4f03b6946e913e62
TechMirko <mirko05.mz@gmail.com>
2023-10-03 11:12:05 +0100

    Update casa_domotica.ino

--- casa_domotica.ino
+++ casa_domotica.ino
@@ -26,6 +26,8 @@ WiFiClient net;
 PubSubClient client(net);
 
 bool statoLed; // stato del led per controllo incrociato
+uint8_t statoFinestra = 0;
+uint8_t statoLuce = 0;
 uint8_t statoVentola = 0; // stato per la ventola per il controllo incrociato e il sync con la dashboard
 uint8_t status = 0; // stato per fare il controllo incrociato con la dashboard
 const char* topicLuceSub = "casa/luce/dash"; // topic per la luce (dashboard --> scheda)
@@ -38,6 +40,10 @@ const char* topicECO2 = "casa/com/eco2";
 const char* topicTVOC = "casa/com/tvoc";
 const char* topicVentola = "casa/com/ventilazione";
 const char* topicVentolaInit = "casa/com/ventola";
+const char* topicModVentola = "casa/mod/ventola";
+const char* topicModFinestra = "casa/mod/finestra";
+const char* topicModeLuce = "casa/mod/luce";
+
 
 void callback(char* topic, uint8_t* payload, unsigned int length) {
   Serial.print("Messaggio arrivato [");
@@ -50,30 +56,89 @@ void callback(char* topic, uint8_t* payload, unsigned int length) {
   }
 
   Serial.print("Comando = "); Serial.println(comando);
-  if(comando == "offLuce") {
+
+  // spengo la luce
+  if(comando == "offLuce" && statoLuce == 1) {
     digitalWrite(LIGHT, LOW);
     statoLed = false;
-  }
+  } else if(comando == "offLuce" && statoLuce == 0) {
+    Serial.println("Comando ignorato, luce in automatico.");
+  } 
 
-  if(comando == "onLuce") {
+  // accendo la luce
+  if(comando == "onLuce" && statoLuce == 1) {
     digitalWrite(LIGHT, HIGH);
     statoLed = true;
+  } else if(comando == "onLuce" && statoLuce == 0) {
+    Serial.println("Comando ignorato, luce in automatico.");
   }
 
-  if(comando == "openWindow") {
+  // apro a finestra
+  if(comando == "openWindow" && statoFinestra == 1) {
     servo.write(0);
+    Serial.println("Finestra aperta (finestra MANUALE)");
+  } else if(comando == "openWindow" && statoFinestra == 0) {
+    Serial.println("Comando ignorato, finestra in automatico.");
   }
 
-  if(comando == "closeWindow") {
+  // chiudo la finestra
+  if(comando == "closeWindow" && statoFinestra == 1) {
     servo.write(90);
+    Serial.println("Finestra chiusa (finestra MANUALE)");
+  }  else if(comando == "closeWindow" && statoFinestra == 0) {
+    Serial.println("Comando ignorato, finestra in automatico.");
   }
 
-  if(comando == "onVentola") {
+  // accendo la ventola SE lo stato della ventola è in manuale
+  if(comando == "onVentola" && statoVentola == 1) {
     digitalWrite(VENTOLA, HIGH);
+    Serial.println("Ventola accesa (ventola MANUALE)");
+  } else if(comando == "onVentola" && statoVentola == 0) {
+    Serial.println("Comando ignorato, ventola in automatico.");
   }
 
-  if(comando == "offVentola") {
+  // spengo la ventola SE lo stato della ventola è in manuale
+  if(comando == "offVentola" && statoVentola == 1) {
     digitalWrite(VENTOLA, LOW);
+    Serial.println("Ventola spenta (ventola MANUALE)");
+  } else if(comando == "offVentola" && statoVentola == 0) {
+    Serial.println("Comando ignorato, ventola in automatico.");
+  }
+
+  // imposto la ventola in modalità manuale (comandata da dashboard)
+  if(comando == "manualVentola") {
+    statoVentola = 1; // ovvero che la ventola viene comandata in manuale
+    Serial.print("Ventola in modalità manuale, statoVentola: "); Serial.println(statoVentola);
+  }
+
+  // imposto  la ventola in modalità automatica (comandata da sensore)
+  if (comando == "autoVentola") {
+    statoVentola = 0; // ovvero la ventola viene comandata dal sensore
+    Serial.print("Ventola in modalità automatica, statoVentola: "); Serial.println(statoVentola); 
+  }
+
+  // imposto la finestra in modalità manuale
+  if(comando == "manualFinestra") {
+    statoFinestra = 1;
+    Serial.print("Finestra in modalità manuale, statoFinestra: "); Serial.println(statoFinestra);
+  }
+
+  // imposto la finestra in modalità automatica
+  if(comando == "autoFinestra") {
+    statoFinestra = 0;
+    Serial.print("Finestra in modalità automatica, statoFinestra: "); Serial.println(statoFinestra);
+  }
+
+  // imposto la luce in modalità automatica
+  if(comando == "autoLuce") {
+    statoLuce = 0;
+    Serial.print("Luce in modalità automatica, statoLuce: "); Serial.println(statoLuce);
+  }
+
+  // imposto la luce in modalità manuale
+  if(comando == "manualLuce") {
+    statoLuce = 1;
+    Serial.print("Luce in modalità manuale, statoLuce: "); Serial.println(statoLuce);
   }
 }
 
@@ -86,7 +151,6 @@ void leggiTemp() {
   dtostrf(tmp, 6, 2, tmpString);
   client.publish(topicHum, humString);
   client.publish(topicTmp, tmpString);
-  /* !!! FARE LA REGOLAZIONE DELLA VENTOLA DISTINGUENDO MANUALE E AUTOMATICO !!!*/
   if(tmp > 26 || hum > 55 && statoVentola == 0) {
     client.publish(topicVentolaInit, "onVentola");
   } 
@@ -103,6 +167,9 @@ void reconnect() {
       client.subscribe(topicLuceSub);
       client.subscribe(topicVentola);
       client.subscribe(topicFinestra);
+      client.subscribe(topicModVentola);
+      client.subscribe(topicModFinestra);
+      client.subscribe(topicModeLuce);
     } else {
       Serial.print("fallito, rc=");
       Serial.print(client.state());
@@ -123,8 +190,8 @@ void airQuality() {
     // Serial.print("eCO2: "); Serial.print(eCO2); Serial.println(" ppm");
     client.publish(topicECO2, eco2String);
     client.publish(topicTVOC, tvocString);
-  } else {
-    Serial.println("Errore durante la lettura del sensore SGP30");
+  // } else {
+  //   Serial.println("Errore durante la lettura del sensore SGP30");
   }
 }
 
@@ -147,10 +214,10 @@ void setup() {
   pinMode(VENTOLA, OUTPUT);
   servo.attach(FINESTRA);
   sgp.begin();
-  if (!sgp.begin()) {
-    Serial.println("Errore durante l'inizializzazione del sensore SGP30");
-    while (1);
-  }
+  // if (!sgp.begin()) {
+  //   Serial.println("Errore durante l'inizializzazione del sensore SGP30");
+  //   while (1);
+  // }
   Serial.println("Inizializzazione completata. Attendi 10 secondi per i dati stabili...");
   client.publish(topicFinestraInit, "closeWindow");
   client.publish(topicLuceInit, "offLuce");
